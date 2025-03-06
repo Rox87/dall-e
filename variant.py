7de51695708703fefb9c972896ee37e1fc21f74d
Rox87 <rodrigohbx@gmail.com>
2025-03-06 07:46:31 -0300

    chore(deps): remove imageGen module and related scripts from project

--- variant.py
+++
@@ -1,7 +0,0 @@
-from model.imageGen import ImageGenerator
-
-imageGen = ImageGenerator()
-
-imageGen.variantImage('images\input\original.png')
-
-imageGen.downloadImage(names=['images\output\leao_var1'])
